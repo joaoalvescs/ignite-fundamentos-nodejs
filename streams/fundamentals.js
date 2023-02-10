f5777df510fccfc3e5af6731631ac84ceb96d631
João Alves <joaoalves.cs@gmail.com>
2023-02-10 16:19:58 -0300

    Criando stream de leitura

---
+++ streams/fundamentals.js
@@ -0,0 +1,27 @@
+// Streams ->
+
+// process.stdin
+//  .pipe(process.stdout)
+
+import { Readable } from 'node:stream'
+
+class OneToHundredStream extends Readable {
+    index = 1
+
+    _read() {
+        const i = this.index++
+
+        setTimeout(() => {
+            if (i > 100) {
+                this.push(null)
+            } else {
+                const buf = Buffer.from(String(i))
+                
+                this.push(buf)
+            }
+        }, 1000)
+    }
+}
+
+new OneToHundredStream()
+    .pipe(process.stdout)
