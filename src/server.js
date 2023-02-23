32b3b629231a176452f3a980f2018f25297834c8
João Alves <joaoalves.cs@gmail.com>
2023-02-23 10:54:05 -0300

    Corpo da requisição em JSON (Stream & Buffers)

--- src/server.js
+++ src/server.js
@@ -1,6 +1,5 @@
 import http from 'node:http'
 
-
 // - Criar usuários
 // - Listagem usuários
 // - Edição de usuários
@@ -23,26 +22,40 @@ import http from 'node:http'
 
 // Stateful - Stateless
 
-// JSON - JavaScript Object Notation
-
 // Cabeçalhos (Requisição/resposta) => Metadados
+
+// HTTP Status Code
 
 const users = []
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
   const { method, url } = req
+
+  const buffers = []
+
+  for await (const chunk of req) {
+    buffers.push(chunk)
+  }
+
+  try {
+    req.body = JSON.parse(Buffer.concat(buffers).toString())
+  } catch {
+    req.body = null
+  }
 
   if (method === 'GET' && url === '/users') {
     return res
-      .setHeader('Content-type', 'application/json')  
+      .setHeader('Content-type', 'application/json')
       .end(JSON.stringify(users))
   }
 
   if (method === 'POST' && url === '/users') {
+    const { name, email } = req.body
+
     users.push({
       id: 1,
-      name: 'John Doe',
-      email: 'johndoe@example.com',
+      name,
+      email,
     })
 
     return res.writeHead(201).end()
